dc26c009ca35472f381a6bafe8c9a2bd726079ba
Tomas Sedlacek <tsedlacek@gmail.com>
2020-07-11 16:51:15 +0200

    got nav bar working

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,3 +1,62 @@
 <script>
+  let isOpen = false;
   export let segment;
+
+  function toggleNav() {
+    isOpen = !isOpen;
+  }
 </script>
+
+<style>
+  @media (min-width: 769px) {
+    nav {
+      border-bottom: solid gray 1px;
+    }
+  }
+</style>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a href="/" class="navbar-item">
+      <img src="logo.jpg" alt="virus" />
+    </a>
+    <span
+      class="navbar-burger burger"
+      class:is-active={isOpen}
+      on:click={toggleNav}
+      aria-label="menu"
+      aria-expanded="false">
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+      <span aria-hidden="true" />
+    </span>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu" class:is-active={isOpen}>
+    <div class="navbar-start">
+      <a class="navbar-item" href="/" class:is-active={segment === undefined}>
+        Home
+      </a>
+
+      <a
+        class="navbar-item"
+        href="/about"
+        class:is-active={segment === 'about'}>
+        About
+      </a>
+    </div>
+
+    <!-- <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</nav>
